167c0977711d29f2e2879f490f698cfc5dccad5f
Gabriel Castillo <castillogabriel005@gmail.com>
2022-12-28 20:54:22 +0100

    Update lwip-tap.c

--- lwip-tap.c
+++ lwip-tap.c
@@ -45,7 +45,8 @@ unsigned char debug_flags = LWIP_DBG_OFF;
 
 #define NETIF_MAX 64
 
-int parse_address(char* addr,struct addrinfo* info,int family) {
+int parse_address(char *addr, struct addrinfo *info, int family)
+{
   struct addrinfo hints;
   struct addrinfo *result;
   struct addrinfo *r;
@@ -56,13 +57,16 @@ int parse_address(char* addr,struct addrinfo* info,int family) {
   hints.ai_socktype = 0;
   hints.ai_protocol = 0;
   hints.ai_flags = AI_NUMERICSERV;
-  res = getaddrinfo(addr,0,&hints,&result);
-  if (res != 0) {
+  res = getaddrinfo(addr, 0, &hints, &result);
+  if (res != 0)
+  {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(res));
     return -1;
   }
-  for (r = result; r != NULL; r = r->ai_next) {
-    if (r->ai_family == family) {
+  for (r = result; r != NULL; r = r->ai_next)
+  {
+    if (r->ai_family == family)
+    {
       *info = *r;
       return 0;
     }
@@ -70,68 +74,84 @@ int parse_address(char* addr,struct addrinfo* info,int family) {
   return -1;
 }
 
-int
-parse_pair(struct tapif* tapif,char* key,char* value)
+int parse_pair(struct tapif *tapif, char *key, char *value)
 {
   struct addrinfo addr;
-  uint8_t* p;
+  uint8_t *p;
 
   if (key == 0 || *key == 0)
     return -1;
 
-#define PARSE_IP4(ip_addr)                                        \
-  do {                                                            \
-    if (value == 0 || *value == 0)                                \
-      return -1;                                                  \
-    if (parse_address(value,&addr,AF_INET) != 0)                  \
-      return -1;                                                  \
-    p = (uint8_t*)&((struct sockaddr_in*)addr.ai_addr)->sin_addr; \
-    IP4_ADDR(&(ip_addr),*p,*(p+1),*(p+2),*(p+3));                 \
-    return 0;                                                     \
-  } while(0)
+#define PARSE_IP4(ip_addr)                                          \
+  do                                                                \
+  {                                                                 \
+    if (value == 0 || *value == 0)                                  \
+      return -1;                                                    \
+    if (parse_address(value, &addr, AF_INET) != 0)                  \
+      return -1;                                                    \
+    p = (uint8_t *)&((struct sockaddr_in *)addr.ai_addr)->sin_addr; \
+    IP4_ADDR(&(ip_addr), *p, *(p + 1), *(p + 2), *(p + 3));         \
+    return 0;                                                       \
+  } while (0)
 
-  if (!strcmp(key,"name")) {
+  if (!strcmp(key, "name"))
+  {
     if (value == 0 || *value == 0)
       return -1;
     tapif->name = value;
     return 0;
-  } else if (!strcmp(key,"addr")) {
+  }
+  else if (!strcmp(key, "addr"))
+  {
     PARSE_IP4(tapif->ip_addr);
-  } else if (!strcmp(key,"netmask")) {
+  }
+  else if (!strcmp(key, "netmask"))
+  {
     PARSE_IP4(tapif->netmask);
-  } else if (!strcmp(key,"gw")) {
+  }
+  else if (!strcmp(key, "gw"))
+  {
     PARSE_IP4(tapif->gw);
-  } else {
+  }
+  else
+  {
     return -1;
   }
 }
 
-int
-parse_interface(struct tapif* tapif,char* param)
+int parse_interface(struct tapif *tapif, char *param)
 {
-  enum {
-    KEY_WAIT, KEY, VALUE_WAIT, VALUE, END
+  enum
+  {
+    KEY_WAIT,
+    KEY,
+    VALUE_WAIT,
+    VALUE,
+    END
   };
   int state = KEY_WAIT;
-  char* p = param;
-  char* key = 0;
-  char* value = 0;
+  char *p = param;
+  char *key = 0;
+  char *value = 0;
 
-  while (state != END) {
-    switch (*p) {
+  while (state != END)
+  {
+    switch (*p)
+    {
     case '\0':
-      if (parse_pair(tapif,key,value) != 0)
+      if (parse_pair(tapif, key, value) != 0)
         return -1;
       state = END;
       break;
     case ',':
-      if (state == KEY_WAIT) {
+      if (state == KEY_WAIT)
+      {
         p++;
         break;
       }
       state = KEY_WAIT;
       *p++ = 0;
-      if (parse_pair(tapif,key,value) != 0)
+      if (parse_pair(tapif, key, value) != 0)
         return -1;
       key = value = 0;
       break;
@@ -148,10 +168,13 @@ parse_interface(struct tapif* tapif,char* param)
       p++;
       break;
     default:
-      if (state == KEY_WAIT) {
+      if (state == KEY_WAIT)
+      {
         state = KEY;
         key = p;
-      } else if (state == VALUE_WAIT) {
+      }
+      else if (state == VALUE_WAIT)
+      {
         state = VALUE;
         value = p;
       }
@@ -161,58 +184,65 @@ parse_interface(struct tapif* tapif,char* param)
   return 0;
 }
 
-void
-help(void)
+void help(void)
 {
 #ifdef LWIP_DEBUG
-  fprintf(stderr,"Usage: lwip-tap [-CEHdh] -i addr=<addr>,netmask=<addr>,name=<name>,gw=<addr> [...]\n");
+  fprintf(stderr, "Usage: lwip-tap [-CEHdh] -i addr=<addr>,netmask=<addr>,name=<name>,gw=<addr> [...]\n");
 #else
-  fprintf(stderr,"Usage: lwip-tap [-CEHh] -i addr=<addr>,netmask=<addr>,name=<name>,gw=<addr> [...]\n");
+  fprintf(stderr, "Usage: lwip-tap [-CEHh] -i addr=<addr>,netmask=<addr>,name=<name>,gw=<addr> [...]\n");
 #endif
   exit(0);
 }
 
 #define IP4_OR_NULL(ip_addr) ((ip_addr).addr == IPADDR_ANY ? 0 : &(ip_addr))
 
-int
-main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
+  printf("InicIalizando la pila LWIP1");
+
   struct tapif tapif[NETIF_MAX];
   struct netif netif[NETIF_MAX];
   int ch;
   int n = 0;
 
-  memset(tapif,0,sizeof(tapif));
-  memset(netif,0,sizeof(netif));
+  memset(tapif, 0, sizeof(tapif));
+  memset(netif, 0, sizeof(netif));
   printf("InicIalizando la pila LWIP1");
-  tcpip_init(NULL,NULL);
+  tcpip_init(NULL, NULL);
 
 #ifdef LWIP_DEBUG
-  while ((ch = getopt(argc,argv,"CEHdhi:")) != -1) {
+  while ((ch = getopt(argc, argv, "CEHdhi:")) != -1)
+  {
 #else
-  while ((ch = getopt(argc,argv,"CEHhi:")) != -1) {
+  while ((ch = getopt(argc, argv, "CEHhi:")) != -1)
+  {
 #endif
-    switch (ch) {
+    switch (ch)
+    {
     case 'C':
       chargen_init();
       break;
     case 'E':
       udpecho_init();
+        printf("InicIalizando la pila LWIP2");
+
       echo_init();
+        printf("InicIalizando la pila LWIP3");
+
       break;
     case 'H':
       http_server_netconn_init();
       break;
 #ifdef LWIP_DEBUG
     case 'd':
-      debug_flags |= (LWIP_DBG_ON|LWIP_DBG_TRACE|LWIP_DBG_STATE|
-                      LWIP_DBG_FRESH|LWIP_DBG_HALT);
+      debug_flags |= (LWIP_DBG_ON | LWIP_DBG_TRACE | LWIP_DBG_STATE |
+                      LWIP_DBG_FRESH | LWIP_DBG_HALT);
       break;
 #endif
     case 'i':
       if (n >= NETIF_MAX)
         break;
-      if (parse_interface(&tapif[n],optarg) != 0)
+      if (parse_interface(&tapif[n], optarg) != 0)
         help();
       netif_add(&netif[n],
                 IP4_OR_NULL(tapif[n].ip_addr),
